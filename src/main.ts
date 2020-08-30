eddaff7414cbacf09609c9a18922421aef7999f1
Jens Ungerer <github@jens-ungerer.de>
2020-08-30 02:41:53 +0200

    some kind of hello world

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
-import { App } from './app/app';
-import { Logger } from './app/common/logger';
-
-Logger.logTask('SYSTEM', 'STARTING');
-
-App.run();
-
-Logger.logTask('SYSTEM', 'FINISHED');
+console.log('time-tracker-server');
