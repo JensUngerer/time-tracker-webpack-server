ca0dc35747133e102e9282dcd49c21148df089c9
Jens Ungerer <github@jens-ungerer.de>
2020-08-30 04:14:18 +0200

    minor changes

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,10 @@ import * as routesConfig from './../../common/typescript/routes.js';
 const port: number = routesConfig.port;
 const hostname = routesConfig.hostname;
 
-// console.log('time-tracker-server');
-
 const app = new App(port, hostname);
-
 app.configure();
 app.configureExpress();
 app.configureRest();
-
 app.setupDatabaseConnection();
 
 AppManager.registerAppClosingEvent(app, true);
